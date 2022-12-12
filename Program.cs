20e14dadf84358ae7a3bbf9366459289c4b1faef
gharjare <gharjarechetan@gmail.com>
2022-12-12 18:54:11 +0530

    wlcm msg

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace JsonProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            JsonProblem.JsonMain json = new JsonProblem.JsonMain();
+            json.convert("C:\\Users\\Achal\\source\\repos\\JsonProblem\\JsonProblem\\jsconfig1.json");
+        }
+    }
+}
